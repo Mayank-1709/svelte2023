161f7b35bc715fb999fc8b711eb930804529289e
Mayank-1709 <negim3@student.op.ac.nz>
2023-09-14 10:21:35 +1200

    Working on the gallery page in the website. Added a structure for my page and downloaded some photos for my page

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,1 +1,22 @@
-<h1> gallery </h1>
+<div class="main-page-content"> 
+   <div class="photo-container">
+        <div class="photo1">
+            <p> </p>
+        </div>
+        <div class="photo2">
+            <p> </p>
+        </div>
+        <div class="photo3">
+            <p> </p>
+        </div>
+        <div class="photo4">
+            <p> </p>
+        </div>
+        <div class="photo5">
+            <p> </p>
+        </div>
+        <div class="photo6">
+            <p> </p>
+        </div>
+   </div> 
+</div>
