dd537d0b0d679fd90997bb1cbe4f235aaf56669e
Mayank-1709 <negim3@student.op.ac.nz>
2023-09-14 11:14:14 +1200

    Added some more images to the static folder to use in the website. Added styling to the structure for the photo gallery page.

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,22 +1,22 @@
 <div class="main-page-content"> 
-   <div class="photo-container">
+   <div class="photo-container-career-highlights">
         <div class="photo1">
-            <p> </p>
+            <p>Ronaldo's football journey started at Sporting Lisbon.</p>
         </div>
         <div class="photo2">
-            <p> </p>
+            <p>In the iconic red jersey, Ronaldo soared to stardom.</p>
         </div>
         <div class="photo3">
-            <p> </p>
+            <p>Real Madrid witnessed Ronaldo's Galactico era.</p>
         </div>
         <div class="photo4">
-            <p> </p>
+            <p>Juventus saw Ronaldo's Serie A dominance.</p>
         </div>
         <div class="photo5">
-            <p> </p>
+            <p>Ronaldo's journey continues with AL-Nassr in Saudi Arabia.</p>
         </div>
         <div class="photo6">
-            <p> </p>
+            <p>Leading Portugal to victory, Ronaldo's captaincy shines.</p>
         </div>
    </div> 
 </div>
