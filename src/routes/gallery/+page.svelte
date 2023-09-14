706a929097634bc9b166d611d83b2e60ad61940e
Mayank-1709 <negim3@student.op.ac.nz>
2023-09-14 21:43:24 +1200

    added photos in the static folder and added the structure for video gallery in the gallery page. Also gave the grid style to video tile and styled the page using css

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -22,4 +22,69 @@
             <p>Leading Portugal to victory, Ronaldo's captaincy shines.</p>
         </div>
    </div> 
+   <div class="photo-container-personal-life">
+        <div class="headings">
+            <h2>PERSONAL LIFE</h2>
+        </div>
+        <div class="gallery">
+            <div class="personal-life-photo photo_1"></div>
+            <div class="description">
+                <p>
+                    Cristiano Ronaldo's late father, José Dinis Aveiro, played a significant role in his life.
+                    Despite his passing in 2005, Cristiano often pays tribute to his father's memory,
+                    crediting him with inspiring his football journey.
+                </p>
+            </div>
+            <div class="description">
+                <p> 
+                    Maria Dolores dos Santos Aveiro is Cristiano Ronaldo's beloved mother. 
+                    Her unwavering support and guidance have been pivotal in shaping Cristiano's career and life.
+                    He holds a deep affection for her.
+                </p>
+            </div>
+            <div class="personal-life-photo photo_2"></div>
+            <div class="personal-life-photo photo_3"></div>
+            <div class="description">
+                <p> 
+                    Georgina Rodríguez is Cristiano Ronaldo's partner and the mother of one of his children, Alana Martina.
+                    They share a close bond and often share glimpses of their life together on social media.
+                </p>
+            </div>
+            <div class="description">
+                <p> 
+                    Cristiano Ronaldo is a doting father to five children. Cristiano Jr., Mateo, Eva Maria, Alana Martina and Bella bring immense joy to his life.
+                    His social media often features heartwarming moments with his kids.
+                </p>
+            </div>
+            <div class="personal-life-photo photo_4"></div>
+            <div class="personal-life-photo photo_5"></div>
+            <div class="description">
+                <p>
+                    Cristiano Ronaldo's dedication to fitness is legendary. His rigorous workout routines, diet, 
+                    and commitment to staying in peak physical condition have made him an inspiration for fitness enthusiasts worldwide. 
+                </p>
+            </div>  
+        </div>
+   </div>
+   <div class="video-container">
+        <div class="headings">
+            <h2>VIDEO GALLERY</h2>
+        </div>
+        <div class="video-gallery">
+            <div class="video-goals">
+                <video autoplay muted loop> 
+                    <source src="../videos/cr7-hero-video.mp4" type="video/mp4">
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+            <div class="video-goals"></div>
+            <div class="video-goals"></div>
+            <div class="video-goals"></div>
+            <div class="video-goals"></div>
+            <div class="video-goals"></div>
+            <div class="video-goals"></div>
+            <div class="video-goals"></div>
+            <div class="video-goals"></div>
+        </div>  
+   </div>
 </div>
