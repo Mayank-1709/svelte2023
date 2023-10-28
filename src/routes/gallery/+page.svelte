431efa8e41c4bb41dd791335196b6aeb2c9255e5
Mayank-1709 <negim3@student.op.ac.nz>
2023-10-28 23:57:34 +1300

    Created a for loop to iterate over the array of careerData and created the HTML structure minimising the use of repetitive code

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -13,24 +13,11 @@
         <div class="headings">
             <h2>CAREER HIGHLIGHTS</h2>
         </div>
-        <div class="photo1">
-            <p>Ronaldo's football journey started at Sporting Lisbon.</p>
-        </div>
-        <div class="photo2">
-            <p>In the iconic red jersey, Ronaldo soared to stardom.</p>
-        </div>
-        <div class="photo3">
-            <p>Real Madrid witnessed Ronaldo's Galactico era.</p>
-        </div>
-        <div class="photo4">
-            <p>Juventus saw Ronaldo's Serie A dominance.</p>
-        </div>
-        <div class="photo5">
-            <p>Ronaldo's journey continues with AL-Nassr in Saudi Arabia.</p>
-        </div>
-        <div class="photo6">
-            <p>Leading Portugal to victory, Ronaldo's captaincy shines.</p>
-        </div>
+        {#each careerData as data, index(index)}
+            <div class="photo{index+1}">
+                <p>{data}</p>
+            </div>
+        {/each}
    </div> 
    <div class="photo-container-personal-life">
         <div class="headings">
