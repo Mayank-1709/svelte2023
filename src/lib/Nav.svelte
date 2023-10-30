a2873530761e136f312856779b2521a2764da3bb
Mayank-1709 <negim3@student.op.ac.nz>
2023-10-31 00:28:13 +1300

    Added comments to the +Nav.svelte page

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,7 +1,14 @@
+<!-- Navigation container for the header -->
 <nav class="nav-header"> 
+     <!-- Unordered list for the navigation menu -->
     <ul class="nav-menu"> 
+        <!-- List item for the 'Home' link with an anchor tag -->
         <li><a href="/">Home</a></li>
+
+        <!-- List item for the 'Gallery' link with an anchor tag -->
         <li><a href="/gallery">Gallery</a></li>
+        
+        <!-- List item for the 'Stats' link with an anchor tag -->
         <li><a href="/stats">Stats</a></li>
     </ul>
 </nav>
