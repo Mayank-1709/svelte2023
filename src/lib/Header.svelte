2ae3038eda0d79662ce7bee83a64331a04151336
Mayank-1709 <negim3@student.op.ac.nz>
2023-10-31 00:32:40 +1300

    Adding comments in the Header.svelte page

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,20 +1,21 @@
 <script>
-    import Nav from '$lib/Nav.svelte' 
+    import Nav from '$lib/Nav.svelte' // Import the 'Nav' component from a Svelte module
 </script>
 <header class="main-header" id="fixed-header">
     <div class=sub-main-header>
         <div class="site-title-container">
-            <div>The</div>
-            <div>Ronaldo</div> 
-            <div>Realm</div>
+            <div>The</div> <!-- The first part of the site title -->
+            <div>Ronaldo</div> <!-- The second part of the site title -->
+            <div>Realm</div> <!-- The third part of the site title -->
         </div>
         <div>
             <div class="football-animation">
+                <!-- Animated football icon with spinning effect -->
             <i class="fa-solid fa-futbol style-icon fa-spin" style="font-size:7em" ></i>
             </div>
         </div>
-        <div class="logo"></div>
-        <div class="nav-container"><Nav /></div>
+        <div class="logo"></div> <!-- Div containing the logo of the website -->
+        <div class="nav-container"><Nav /></div> <!-- Navigation container using the 'Nav' component -->
     </div>
 </header>
 
