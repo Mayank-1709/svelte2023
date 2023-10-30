151f781ee43adbc595029891221bdc1261248d1a
Mayank-1709 <negim3@student.op.ac.nz>
2023-10-31 00:37:47 +1300

    Adding comments to the Footer.svelte page

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,20 +1,24 @@
 <footer class="footer-container"> 
-    <div></div>
+    <div></div><!-- An empty div, which can be used for additional content if needed -->
     <div class="footer-elements">
         <div class="copyright">
-            <h4>Copyright</h4>
+            <h4>Copyright</h4> <!-- Copyright label -->
             <i class="fa-solid fa-copyright" style="font-size: 1.5em"></i>
-            <h4>MayankNegi</h4>
+            <h4>MayankNegi</h4> <!-- Username -->
+
         </div>
         <div>
+            <!-- Facebook icon with a link to Cristiano Ronaldo's Facebook page -->
             <a href="https://www.facebook.com/Cristiano/" target="_blank">
             <i class="fa-brands fa-facebook" style="font-size: 3em"></i>
         </div>
         <div>
+             <!-- Instagram icon with a link to Cristiano Ronaldo's Instagram page -->
             <a href="https://www.instagram.com/cristiano/" target="_blank">
             <i class="fa-brands fa-instagram" style="font-size: 3em"></i>
         </div>
         <div>
+            <!-- Twitter icon with a link to Cristiano Ronaldo's Twitter page -->
             <a href="https://twitter.com/Cristiano" target="_blank">
             <i class="fa-brands fa-square-twitter" style="font-size: 3em"></i>
         </div>
