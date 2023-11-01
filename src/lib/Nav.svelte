284239617b0780674e3ddc264136899163955c53
Mayank-1709 <negim3@student.op.ac.nz>
2023-11-02 01:18:37 +1300

    changed the animation in the home page image description, changed comments in Nav component and changed the data representation in Statsdata.json

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -2,13 +2,13 @@
 <nav class="nav-header"> 
      <!-- Unordered list for the navigation menu -->
     <ul class="nav-menu"> 
-        <!-- List item for the 'Home' link with an anchor tag -->
+        <!-- List item for the 'Home' link -->
         <li><a href="/">Home</a></li>
 
-        <!-- List item for the 'Gallery' link with an anchor tag -->
+        <!-- List item for the 'Gallery' link -->
         <li><a href="/gallery">Gallery</a></li>
         
-        <!-- List item for the 'Stats' link with an anchor tag -->
+        <!-- List item for the 'Stats' link -->
         <li><a href="/stats">Stats</a></li>
     </ul>
 </nav>
