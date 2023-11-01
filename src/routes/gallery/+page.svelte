0cfcccac8a4953934f23e36f9dfbb50b1568f512
Mayank-1709 <negim3@student.op.ac.nz>
2023-11-01 13:26:40 +1300

    created a json file to hold the family members data to make the code in the gallery page look cleaner, imported the json object on the gallery page to display asintended

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import { onMount } from "svelte";
   import careerData from "../../lib/Careerdata.json";
+  import familyMembers from "../../lib/Familymembers.json";
 
   let urls = [];
   const BASE_URL = `https://api.unsplash.com`;
@@ -32,59 +33,7 @@
     }
 
     urls = tempUrls;
-    console.log(urls);
   });
-
-  // Data for career highlights section
-//   const careerData = 
-//   [
-//     "Ronaldo's football journey started at Sporting Lisbon.",
-//     "In the iconic red jersey, Ronaldo soared to stardom.",
-//     "Real Madrid witnessed Ronaldo's Galactico era.",
-//     "Juventus saw Ronaldo's Serie A dominance.",
-//     "Ronaldo's journey continues with AL-Nassr in Saudi Arabia.",
-//     "Leading Portugal to victory, Ronaldo's captaincy shines.",
-//   ];
-
-  // Data for personal life section
-  let familyMembers = [
-    {
-      name: "José Dinis Aveiro",
-      description: `Cristiano Ronaldo's late father, José Dinis Aveiro, played a significant role in his life.
-                          Despite his passing in 2005, Cristiano often pays tribute to his father's memory,
-                          crediting him with inspiring his football journey.`,
-      photoClass: "photo_1",
-    },
-
-    {
-      name: "Maria Dolores dos Santos Aveiro",
-      description: `Maria Dolores dos Santos Aveiro is Cristiano Ronaldo's beloved mother. 
-                          Her unwavering support and guidance have been pivotal in shaping Cristiano's career and life.
-                          He holds a deep affection for her.`,
-      photoClass: "photo_2",
-    },
-
-    {
-      name: "Georgina Rodríguez",
-      description: `Georgina Rodríguez is Cristiano Ronaldo's partner and the mother of one of his children, Alana Martina.
-                          They share a close bond and often share glimpses of their life together on social media.`,
-      photoClass: "photo_3",
-    },
-
-    {
-      name: "Cristiano Ronaldo's Children",
-      description: `Cristiano Ronaldo is a doting father to five children. Cristiano Jr., Mateo, Eva Maria, Alana Martina and Bella bring immense joy to his life.
-                          His social media often features heartwarming moments with his kids.`,
-      photoClass: "photo_4",
-    },
-
-    {
-      name: "Fitness",
-      description: `Cristiano Ronaldo's dedication to fitness is legendary. His rigorous workout routines, diet, 
-                          and commitment to staying in peak physical condition have made him an inspiration for fitness enthusiasts worldwide. `,
-      photoClass: "photo_5",
-    },
-  ];
 </script>
 
 <!-- The main content of the webpage -->
@@ -135,7 +84,7 @@
       <!-- Title for the section -->
     </div>
     <div class="video-gallery">
-      {#each urls as url, index (index)}
+      {#each urls as url}
         {#if (url.match('.mp4'))}
           <div class="video-goals">
             <!-- Display video with controls and autoplay behavior -->
