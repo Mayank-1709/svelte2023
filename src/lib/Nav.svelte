522d5450a8c36d2d88f9f0be8fdcce3f13de6d2c
Mayank-1709 <negim3@student.op.ac.nz>
2023-11-07 23:30:14 +1300

    Modified the Nav elements to be highlighted when selected, added css and click event

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,9 +1,16 @@
+<script>
+    let selectedNavItem = "Home";
+  
+    function selectNavItem(item) {
+      selectedNavItem = item;
+    }
+</script>
 <!-- Navigation container for the header -->
 <nav class="nav-header"> 
      <!-- Unordered list for the navigation menu -->
     <ul class="nav-menu"> 
-        <li><a href="/">Home</a></li>
-        <li><a href="/gallery">Gallery</a></li>
-        <li><a href="/stats">Stats</a></li>
+        <li><a href="/" on:click={() => selectNavItem("Home")} class:selected={selectedNavItem === "Home"}>Home</a></li>
+        <li><a href="/gallery" on:click={() => selectNavItem("Gallery")} class:selected={selectedNavItem === "Gallery"}>Gallery</a></li>
+        <li><a href="/stats" on:click={() => selectNavItem("Stats")} class:selected={selectedNavItem === "Stats"}>Stats</a></li>
     </ul>
 </nav>
